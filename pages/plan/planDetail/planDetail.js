d4252b0b22454dd74e8c186aa3d731dcf2ab45ca
wushi91 <l>
2018-01-29 22:22:01 +0800

    little change

--- pages/plan/planDetail/planDetail.js
+++ pages/plan/planDetail/planDetail.js
@@ -8,6 +8,27 @@ Page({
     plan:''
   },
 
+
+
+  showDeleteThePlan: function () {
+    wx.showModal({
+      title: '确认删除',
+      content: '删除该日程之后将不可恢复',
+      cancelText: '确定',
+      cancelColor: '#F24949',
+      confirmText: '取消',
+      confirmColor: '#000000',
+      success: res => {
+        if (res.confirm) {
+          //这里是取消，因为调换了按钮的位置
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          //这里是确定
+          this.deleteTheRenter(this.data.userId, this.data.bookid)
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
